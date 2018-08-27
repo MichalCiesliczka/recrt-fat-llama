62c933a93fb4fd96e8fa4a01345faa56d0ad46ac
Michal Ciesliczka <michal.ciesliczka@gmail.com>
2018-08-27 23:21:17 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  extends: [
+    "airbnb",
+  ],
+  parser: "babel-eslint",
+  env: {
+    browser: true
+  },
+  rules: {
+    // "curly": [
+    //   "error"
+    // ],
+    "arrow-body-style": [
+      "off"
+    ],
+    "react/jsx-filename-extension": [
+      "off"
+    ],
+    "no-plusplus": [
+      "error",
+      {
+        "allowForLoopAfterthoughts": true
+      }
+    ]
+  },
+  overrides: [
+    {
+      files: [
+        "**/*.test.js"
+      ],
+      env: {
+        jest: true
+      },
+    },
+  ],
+};
